b7a951e7b462ecbe49769fab6521bf9bdac3b198
Brendan O'Brien <brendan.o'brien@iress.com>
2021-12-01 09:13:50 +1100

    added notes

--- 07-Terraform-State/07-01-Terraform-Remote-State-Storage-and-Locking/terraform-manifests/c1-versions.tf
+++ 07-Terraform-State/07-01-Terraform-Remote-State-Storage-and-Locking/terraform-manifests/c1-versions.tf
@@ -1,6 +1,6 @@
 # Terraform Block
 terraform {
-  required_version = "~> 0.14" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  required_version = "~> 1.0.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -11,12 +11,12 @@ terraform {
   backend "s3" {
     bucket = "terraform-stacksimplify"
     key    = "dev/terraform.tfstate"
-    region = "us-east-1" 
+    region = "us-east-1"
 /*
-    # Enable during Step-09     
+    # Enable during Step-09
     # For State Locking
-    dynamodb_table = "terraform-dev-state-table"    
-*/    
+    dynamodb_table = "terraform-dev-state-table"
+*/
   }
 }
 
@@ -26,6 +26,6 @@ provider "aws" {
   profile = "default"
 }
 /*
-Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
+Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal
 $HOME/.aws/credentials
 */
